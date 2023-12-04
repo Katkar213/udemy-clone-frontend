0b3766e7bfe0c7be3be255d8f5fc0946857ffff5
Katkar213 <blackship213@gmail.com>
2023-12-04 17:28:41 +0530

    all

--- src/Components/SearchProduct.js
+++ src/Components/SearchProduct.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { useLocation } from 'react-router-dom'
+import axios from "axios"
 import "../Components/Cart/Cart.css"
 const SearchProduct = () => {
 const data=useLocation().state
+const[data2,setData2]=useState()
 
+const handleCart=async(item)=>{
+  const existdata=data2.find((items)=>items.id===item.id)
+  console.log(data2)
+   console.log(existdata)
+   if(existdata){
+     alert("data exist");
+   }
+   else{
+     await axios.post("https://udemy-backend-tzzj.onrender.com/api/addToCart",item)
+    
+   }
+   axios.get("https://udemy-backend-tzzj.onrender.com/api/getcartdata")
+   .then((res)=>{
+  setData2(res.data)})
+ 
+ }
   return (
     <div>
     <h1>Search results:{data.length}</h1>
@@ -24,8 +42,9 @@ const data=useLocation().state
           </div>
           {/* <button onClick={handleremove}>Delete</button> */}
         </div>
-        
+        <div className="addtocart-btn"><button className="addtocart" onClick={()=>handleCart(item)}>Add to cart</button></div>
             </div>
+            
       )
     })}
   </div>
